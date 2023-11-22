2188482bb4dcd91361a565525bffad432281f597
Zzyzzx2 <rahanshankar38@gmail.com>
2023-11-22 12:35:15 +0530

    Fixed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex justify-center mt-10 bg-slate-600">
-      <footer className="text-center text-sky-500 text-lg">
+      <footer className="text-center text-sky-500 text-lg fixed bottom-0">
         Made By Rahan
       </footer>
     </div>
